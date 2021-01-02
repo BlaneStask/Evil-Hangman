e7ad1cc5cf5e95f8b8fe2fdd604d47ae7b1b1659
Blane Staskiewicz <bas6248@psu.edu>
2021-01-02 11:38:20 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -7,32 +7,32 @@ int main(){
     list wordlist("words");
 
     cout << "Welcome to Hangman!!!\n\n";
-    cout << "In this easy game of Hangman, you are given " << MAX_GUESSES << " wrong guesses!\n";
+    cout << "In this easy game of Hangman, you are given " << MAX_GUESSES << " wrong guesses!" << endl;
     string guessedLetters = "";
     int wrongCount = 0;
     bool won = false;
 
-    while (wrongCount < MAX_GUESSES && !won){
+    while(wrongCount < MAX_GUESSES && !won){
         cout << "Word: " << wordlist.mask() << endl;
         cout << "Guess a letter: ";
         char letter;
         cin >> letter;
-        while (guessedLetters.find(letter) != string::npos){
+        while(guessedLetters.find(letter) != string::npos){
             cout << "You have already guessed " << letter << "!\nPlease try again: ";
             cin >> letter;
         }
         guessedLetters += letter;
-        if (!wordlist.split(letter)) {
+        if(!wordlist.split(letter)) {
             wrongCount++;
-            cout << "You were wrong! " << (MAX_GUESSES - wrongCount) << " chances left!\n";
+            cout << "You were wrong! " << (MAX_GUESSES - wrongCount) << " chances left!" << endl;
         }
         won = wordlist.is_guessed();
     }
-    if (won){
-        cout << "You won!  The word was '" << wordlist.word() << "'\n";
+    if(won){
+        cout << "You won!  The word was '" << wordlist.word() << endl;
     }
     else{
-        cout << "You lost!  The word was '" << wordlist.word() << "'\n";
+        cout << "You lost!  The word was '" << wordlist.word() << endl;
     }
     return 0;
 }
